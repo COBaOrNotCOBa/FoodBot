691ca9985c4ab0544aaa6b89464a2f82061a8c97
COBaOrNotCOBa <yureal1986@mail.ru>
2024-01-30 16:46:07 +0300

    процесс идёт

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,14 @@ import kotlinx.serialization.json.Json
 fun main(args: Array<String>) {
 //исходные данные
     val tokenBotTg = args[0]
+    val tokenBotAt = args[1]
+    val baseIdAt = args[2]
+    val tableIdAt = args[3]
     var lastUpdateId = 0L
     val json = Json { ignoreUnknownKeys = true }
+//создаем класс таблицы из АТ
+    val airtable = Airtable(tokenBotAt, baseIdAt, tableIdAt, json)
+
 //меню команд в телеграмме
     botCommand(
         json, tokenBotTg, listOf(BotCommand("start", "Глвное меню"))
@@ -14,13 +20,13 @@ fun main(args: Array<String>) {
         Thread.sleep(2500)
         val resultTg = runCatching { getUpdates(tokenBotTg, lastUpdateId) }
         val responseStringTg = resultTg.getOrNull() ?: continue
-        if (responseStringTg!="{\"ok\":true,\"result\":[]}") println(responseStringTg)
-
+        if (responseStringTg != "{\"ok\":true,\"result\":[]}") println(responseStringTg)
+//если ошибка ждём дополнительно 5 секунд, на последний запрос нет ответа
         if (responseStringTg.contains("error_code")) {
             Thread.sleep(5000)
             continue
         }
-
+//получаем список апдейтов и проверяем не пустые ли они. После поочереди обрабатываем
         val responseTg: ResponseTg = json.decodeFromString(responseStringTg)
         if (responseTg.result.isEmpty()) continue
         val sortedUpdates = responseTg.result.sortedBy { it.updateId }
@@ -34,7 +40,7 @@ fun main(args: Array<String>) {
         }
     }
 }
-
+//разбиваем апдейт на куски
 fun handleUpdate(
     updateTg: Update,
     json: Json,
@@ -43,7 +49,7 @@ fun handleUpdate(
     val message = updateTg.message?.text ?: ""
     val chatId = updateTg.message?.chat?.id ?: updateTg.callbackQuery?.message?.chat?.id ?: return
     val data = updateTg.callbackQuery?.data ?: ""
-
+//обрабатываем команду или сообщение от пользователя
     when {
 //Стартовое меню
         message.lowercase() == MAIN_MENU || data == MAIN_MENU -> {
@@ -53,9 +59,11 @@ fun handleUpdate(
         data == "1" -> {
             sendMessage(json, botTokenTg, chatId, "1")
         }
+
         data == "2" -> {
             sendMessage(json, botTokenTg, chatId, "2")
         }
+
         data == "3" -> {
             sendMessage(json, botTokenTg, chatId, "3")
         }
