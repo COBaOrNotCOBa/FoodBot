d78845e63feac5f3a67384a8780fd0e74cbf591c
COBaOrNotCOBa <yureal1986@mail.ru>
2024-02-12 16:06:22 +0300

    PDF в TODO

--- src/main/kotlin/PdfCreator.kt
+++ src/main/kotlin/PdfCreator.kt
@@ -10,17 +10,17 @@ import java.io.FileOutputStream
 class FilePdf() {
 //создаем PDF файл с текстом
 
-    fun createPdf(filePath: String, content: String): File {
-        val pdf = PdfDocument(PdfWriter(FileOutputStream(filePath)))
-        val document = Document(pdf)
-
-        // Указываем поддержку кириллицы
-        val font = PdfFontFactory.createFont("path_to_your_cyrillic_font.ttf", PdfEncodings.IDENTITY_H, true)
-
-        document.setFont(font) // устанавливаем шрифт для документа
-        document.add(Paragraph(content))
-        document.close()
-
-        return File(filePath)
-    }
+//    fun createPdf(filePath: String, content: String): File {
+//        val pdf = PdfDocument(PdfWriter(FileOutputStream(filePath)))
+//        val document = Document(pdf)
+//
+//        // Указываем поддержку кириллицы
+//        val font = PdfFontFactory.createFont("path_to_your_cyrillic_font.ttf", PdfEncodings.IDENTITY_H, true)
+//
+//        document.setFont(font) // устанавливаем шрифт для документа
+//        document.add(Paragraph(content))
+//        document.close()
+//
+//        return File(filePath)
+//    }
 }
