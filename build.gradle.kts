4026a85616225133a2640ed71a54cd062b2af413
COBaOrNotCOBa <yureal1986@mail.ru>
2024-02-12 14:36:23 +0300

    подключился к yandexGPT,создал пормпт с в jsonе для запроса. Попробовал подключить библиотеку для создания  PDF файла

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.itextpdf:itext-core:8.0.2")
 }
 
 tasks.test {
