3a0850ea09e32a6ccca515e5c976400c8cf1c7b9
COBaOrNotCOBa <yureal1986@mail.ru>
2024-08-29 10:14:15 +0300

    sqldelight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+    id("app.cash.sqldelight") version "2.0.2"
     application
 }
 
@@ -17,6 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1-Beta")
+    implementation("app.cash.sqldelight:sqlite-driver:2.0.2")
 }
 
 tasks.test {
@@ -24,10 +26,19 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(17)
 }
 
 application {
     mainClass.set("MainKt")
 }
 
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.databases")
+        }
+    }
+}
+
+//./gradlew wrapper --gradle-version latest
